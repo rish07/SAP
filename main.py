7cf4be5b4ee055f0b86bb25f37c966bf9b647f3a
TheCreator01 <rishirajsinghchhabra@gmail.com>
2019-09-01 00:30:58 +0530

    Recog temp2

--- main.py
+++ main.py
@@ -1,14 +1,52 @@
+#!/usr/bin/python
 import speech_recognition as sr
+import pyaudio
+import wave
 
-r= sr.Recognizer()
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+RECORD_SECONDS = 5
+WAVE_OUTPUT_FILENAME = "output.wav"
 
-with sr.Microphone() as source:
-    print('Speak the command')
-    audio = r.listen(source)
+p = pyaudio.PyAudio()
 
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
 
-    try:
-        text = r.recognize_google(audio)
-        print('You said: {}'.format(text))
-    except:
-        print('Please speak again')
+print("* recording")
+
+frames = []
+
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    frames.append(data)
+
+print("* done recording")
+
+stream.stop_stream()
+stream.close()
+p.terminate()
+
+wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+wf.setnchannels(CHANNELS)
+wf.setsampwidth(p.get_sample_size(FORMAT))
+wf.setframerate(RATE)
+wf.writeframes(b''.join(frames))
+wf.close()
+
+r = sr.Recognizer()
+output = sr.AudioFile('output.wav')
+with output as source:
+    r.adjust_for_ambient_noise(source, duration=0.5)
+    audio = r.record(source)
+
+try:
+    text = r.recognize_google(audio)
+    print('Did you just say :', format(text))
+except Exception:
+    print('Sorry ! did not catch that. Shall we try again ? ')
