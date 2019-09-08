e4ee6e470575e3f2ea118c5acf624dd1f8620fb6
Rishi Raj Singh <49035175+rish07@users.noreply.github.com>
2019-09-08 12:45:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 from processing import recog
+import speech_recognition as sr
+import pyttsx3 
+jarvis = pyttsx3.init()
 while(True):
-    recog()
+    print('listening')
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        
+        audio = r.listen(source)
+        
+
+        s = (r.recognize_google(audio))
+        message = s.lower()
+        print(message)
+        if (message == 'hey jarvis' or message == 'hi jarvis'):
+            recog()
+        if(message == 'goodbye' or message == 'good bye'):
+            jarvis.say('Goodbye,Sir!')
+            jarvis.runAndWait()
+            break
