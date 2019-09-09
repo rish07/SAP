b239e63e133a43577e42b861c37dab1abaac637b
noob-master147 <divyanshkhandelwal147@gmail.com>
2019-09-09 11:28:45 +0530

    configured GPIO PINOUT for the relay module

--- raspi.py
+++ raspi.py
@@ -1,14 +1,35 @@
 import time
+
 from firebase import firebase
 firebase = firebase.FirebaseApplication('https://sapj01.firebaseio.com/')
+
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print("Error importing RPi.GPIO!. You can achieve this by using 'sudo' to run your script")
+GPIO.setmode(GPIO.BOARD)
+
+
+GPIO.setup(29, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(31, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(33, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(35, GPIO.OUT, initial=GPIO.LOW)
+
+
 
 while True:
     
 
-    fan = firebase.get('/','fan')
-    tv = firebase.get('/','tv')
-    music = firebase.get('/','music')
-    light = firebase.get('/','light')
+    fanState = firebase.get('/','fan')
+    tvState = firebase.get('/','tv')
+    musicState = firebase.get('/','music')
+    lightState = firebase.get('/','light')
+
+    GPIO.output(29, fanState)
+    GPIO.output(31, tvState)
+    GPIO.output(33, musicState)
+    GPIO.output(35, lightState)
+    
 
     print(fan," ",tv," ",music," ",light)
 
