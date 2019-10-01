445b26b08483048ca3942127c1c77708ac639f34
Rishi Raj Singh <creator@pop-os.localdomain>
2019-10-01 22:13:22 +0530

    deleted raspi

--- main.py
+++ main.py
@@ -1,33 +1,33 @@
 from processing import recog
-# import speech_recognition as sr
-# import pyttsx3 
-# sophia = pyttsx3.init()
-# voices = sophia.getProperty('voices')
-# sophia.setProperty('voice', voices[1].id)
-# while(True):
-#     print('listening')
-#     r = sr.Recognizer()
-#     with sr.Microphone() as source:
+import speech_recognition as sr
+import pyttsx3 
+sophia = pyttsx3.init()
+voices = sophia.getProperty('voices')
+sophia.setProperty('voice', voices[1].id)
+while(True):
+    print('listening')
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
         
-#         audio = r.listen(source)
+        audio = r.listen(source)
         
-#         #sophia
-#         s = (r.recognize_google(audio))
-#         message = s.lower()
-#         print(message)
-#         u = 'sofia'
-#         if u in message:
-#             message = 'sophia'
-#         if ('sophia') in message:
-#             recog()
-#         if(message == 'goodbye' or message == 'good bye' or message == 'no'):
-#             sophia.say('Goodbye,Sir!')
-#             sophia.runAndWait()
-#             break
-#         if(message == 'thank you' or message == 'thankyou'):
-#             sophia.say('Anything else sir?')
-#             sophia.runAndWait()
-#         if(message == 'yes'):
-#             recog()
+        #sophia
+        s = (r.recognize_google(audio))
+        message = s.lower()
+        print(message)
+        u = 'sofia'
+        if u in message:
+            message = 'sophia'
+        if ('sophia') in message:
+            recog()
+        if(message == 'goodbye' or message == 'good bye' or message == 'no'):
+            sophia.say('Goodbye,Sir!')
+            sophia.runAndWait()
+            break
+        if(message == 'thank you' or message == 'thankyou'):
+            sophia.say('Anything else sir?')
+            sophia.runAndWait()
+        if(message == 'yes'):
+            recog()
 while(True):
     recog()
